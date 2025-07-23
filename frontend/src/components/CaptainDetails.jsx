ad2c4926533457d050ee52fd54a747e01cbd97c1
Yashpal <jaatyashpal4011@gmail.com>
2025-07-23 22:46:18 +0530

    Initial commit

--- frontend/src/components/CaptainDetails.jsx
+++ frontend/src/components/CaptainDetails.jsx
@@ -21,17 +21,17 @@ const CaptainDetails = () => {
             <div className='flex p-3 mt-8 bg-gray-100 rounded-xl justify-center gap-5 items-start'>
                 <div className='text-center'>
                     <i className="text-3xl mb-2 font-thin ri-timer-2-line"></i>
-                    <h5 className='text-lg font-medium'>10.2</h5>
+                    <h5 className='text-lg font-medium'>10.3</h5>
                     <p className='text-sm text-gray-600'>Hours Online</p>
                 </div>
                 <div className='text-center'>
                     <i className="text-3xl mb-2 font-thin ri-speed-up-line"></i>
-                    <h5 className='text-lg font-medium'>10.2</h5>
+                    <h5 className='text-lg font-medium'>10.3</h5>
                     <p className='text-sm text-gray-600'>Hours Online</p>
                 </div>
                 <div className='text-center'>
                     <i className="text-3xl mb-2 font-thin ri-booklet-line"></i>
-                    <h5 className='text-lg font-medium'>10.2</h5>
+                    <h5 className='text-lg font-medium'>10.3</h5>
                     <p className='text-sm text-gray-600'>Hours Online</p>
                 </div>
 
